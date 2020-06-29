2f540947c077e4b352a81a3e345114aeb68ec9d0
allanvobraun <allanvobraun@gmail.com>
2020-06-29 18:16:02 -0300

    Topbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import ExampleComponent from './components/ExampleComponent.vue';
+import TopBar from './components/TopBar.vue';
 require('./bootstrap');
+
+window.Vue = require('vue');
+import { BootstrapVue } from 'bootstrap-vue';
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.use(BootstrapVue);
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        ExampleComponent,
+        TopBar
+    }
+});
