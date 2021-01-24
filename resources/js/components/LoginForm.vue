0bd25165e2b7908e899d1cfdc34647fad0b84075
allanbraun <allanvobraun@gmail.com>
2021-01-24 16:33:22 -0300

    Formulario contruido

---
+++ resources/js/components/LoginForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div>
+      <b-input-group size="md" class="my-3">
+        <b-input-group-prepend>
+          <b-input-group-text class="login-input-append">
+            <b-icon-envelope-fill variant="dark"></b-icon-envelope-fill>
+          </b-input-group-text>
+        </b-input-group-prepend>
+        <b-form-input
+          id="username"
+          v-model="form.email"
+          type="email"
+          placeholder="Email"
+        ></b-form-input>
+      </b-input-group>
+      <b-input-group size="md" class="my-3">
+        <b-input-group-prepend>
+          <b-input-group-text class="login-input-append">
+            <b-icon-key-fill variant="dark"></b-icon-key-fill>
+          </b-input-group-text>
+        </b-input-group-prepend>
+        <b-form-input
+          id="password"
+          v-model="form.password"
+          type="password"
+          placeholder="Senha"
+        ></b-form-input>
+      </b-input-group>
+    </div>
+    <div class="d-flex justify-content-center align-content-center">
+      <b-button size="lg" variant="primary">Login</b-button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import {BIconEnvelopeFill, BIconKeyFill} from 'bootstrap-vue';
+
+export default {
+  components: {BIconEnvelopeFill, BIconKeyFill},
+  data() {
+    return {
+      form: {
+        email: '',
+        password: ''
+
+      }
+    }
+  }
+}
+</script>
--- resources/js/components/UserLogin.vue
+++ resources/js/components/UserLogin.vue
@@ -1,13 +1,23 @@
 <template>
-  <h1>aaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h1>
+  <b-container fluid class="h-100 d-flex flex-column justify-content-center align-content-center">
+    <b-row align-h="center" align-v="center">
+      <b-col cols="4">
+        <b-card>
+          <template #header>
+            <h3 class="mb-0">Conecte-se</h3>
+          </template>
+          <login-form></login-form>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
+import LoginForm from "components/LoginForm";
+
 export default {
-name: "UserLogin"
+  name: "UserLogin",
+  components: {LoginForm}
 }
 </script>
-
-<style scoped>
-
-</style>
