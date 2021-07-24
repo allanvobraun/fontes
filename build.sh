17d5913f20bd9efde6d194a059d016addf149662
allanbraun <allanvobraun@gmail.com>
2021-07-24 13:59:16 -0300

    Echo no script

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 composer install --no-dev
+echo 'composer finalzado'
 yarn
+echo 'instalação de modulos finalizada'
 yarn run prod
+echo 'build do yarn finalzado'
 php artisan migrate
+echo 'migração do banco finalizada'
